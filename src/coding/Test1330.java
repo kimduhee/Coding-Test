7e7a569a762935c141d25ecefb9ae36a38266276
test <oo2706@naver.com>
2022-07-11 22:01:22 +0900

    파일수정

--- src/coding/Test1330.java
+++ src/coding/Test1330.java
@@ -3,6 +3,11 @@ package coding;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/**
+ * A가 B보다 큰 경우에는 '>'를 출력
+ * A가 B보다 작은 경우에는 '<'를 출력
+ * A와 B가 같은 경우에는 '=='를 출력
+ */
 public class Test1330 {
 
 	public static void main(String[] args) throws Exception {
--- src/coding/Test9498.java
+++ src/coding/Test9498.java
@@ -3,6 +3,14 @@ package coding;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/**
+ * 90 ~ 100점은 A, 
+ * 80 ~ 89점은 B, 
+ * 70 ~ 79점은 C, 
+ * 60 ~ 69점은 D, 
+ * 나머지 점수는 F를 출력
+ *
+ */
 public class Test9498 {
 
 	public static void main(String[] args) throws Exception{
