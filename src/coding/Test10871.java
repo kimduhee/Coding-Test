ce382a0c6e53f320e49886732ef905c66d315c36
test <oo2706@naver.com>
2022-07-15 22:57:44 +0900

    수정

--- src/coding/Test10871.java
+++ src/coding/Test10871.java
@@ -3,6 +3,16 @@ package coding;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/**
+ * 정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 
+ * 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
+ * 
+ * 입력
+ * 10 5
+ * 1 10 4 9 2 3 8 5 7 6
+ * 출력
+ * 1 4 2 3
+ */
 public class Test10871 {
 
 	public static void main(String[] args) throws Exception{
--- src/coding/Test10950.java
+++ src/coding/Test10950.java
@@ -3,6 +3,23 @@ package coding;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/**
+ * 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ * 
+ * 입력
+ * 5
+ * 1 1
+ * 2 3
+ * 3 4
+ * 9 8
+ * 5 2
+ * 출력
+ * 2
+ * 5
+ * 7
+ * 17
+ * 7
+ */
 public class Test10950 {
 
 	public static void main(String[] args) throws Exception{
